08017dc3453059de8720303e6fd3e471192aa630
rizwan <rizwanriaz1988@gmail.com>
2023-10-09 23:01:47 +0500

    button added

--- Quarter 02/hello_nextjs_rizi/src/app/page.tsx
+++ Quarter 02/hello_nextjs_rizi/src/app/page.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+const ButtonSample2 = () => {
   return (
-        <div>Hello World by Rizwan Riaz</div>
+    // <div className="flex min-h-screen flex-col justify-center items-center gap-9 py-8 sm:py-2 px-3 bg-gray-300">
+    <div className="flex min-h-screen  justify-center items-center   bg-gray-300">
+      {/* <div> */}
+      <button className="py-3 rounded-full border-2 bg-black px-6 text-xs w-32 hover:bg-white hover:text-black hover:border-2 hover:border-[black] font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none active:shadow-lg">
+        Update
+      </button>
+      <button className="py-3 rounded-full border-2 bg-black px-6 text-xs w-32 hover:bg-white hover:text-black hover:border-2 hover:border-[black] font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none active:shadow-lg">
+        Save
+      </button>
+      <button className="py-3 rounded-full border-2 bg-black px-6 text-xs w-32 hover:bg-white hover:text-black hover:border-2 hover:border-[black] font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none active:shadow-lg">
+        Exit
+      </button>
+      {/* </div> */}
+    </div>
   );
-}
+};
+export default ButtonSample2;

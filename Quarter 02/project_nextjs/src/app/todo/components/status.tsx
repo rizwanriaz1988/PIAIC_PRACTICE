bc57dcc248cb4a5659fd78275494751d9c87be1a
rizwan <rizwanriaz1988@gmail.com>
2023-12-11 22:59:41 +0500

    see more button issue resolved completely

--- Quarter 02/project_nextjs/src/app/todo/components/status.tsx
+++ Quarter 02/project_nextjs/src/app/todo/components/status.tsx
@@ -1,14 +1,12 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { SetStateAction, useEffect, useRef, useState } from 'react';
-import { Checkbox } from "@/components/ui/checkbox"
-import { RiDeleteBinLine  } from "react-icons/ri";
+"use client";
+import { Button } from "@/components/ui/button";
+import { useEffect, useRef, useState } from "react";
+import { RiDeleteBinLine } from "react-icons/ri";
 import { FiEdit } from "react-icons/fi";
-import { title } from 'process';
-import Todo from '../page';
-import Newtask from './newtask';
+import Todo from "../page";
+import Newtask from "./newtask";
 
-interface Todo  {
+interface Todo {
   id: number;
   title: string;
   description: string;
@@ -17,240 +15,262 @@ interface Todo  {
 
 interface todolist_type {
   todoList: string;
-  
- 
 }
-let data:any ;
-const TodoList = (props:todolist_type) => {
-
+let data: any;
+const TodoList = (props: todolist_type) => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [btnState, setbtnState] = useState<boolean>(false);
   const [btnId, setbtnId] = useState<number>();
-  const [editData, seteditData] = useState<Todo>({ title: "", description: "", status:false,id:0 })
+  const [editData, seteditData] = useState<Todo>({
+    title: "",
+    description: "",
+    status: false,
+    id: 0,
+  });
   const [formVisible, setFormVisible] = useState<boolean>(false);
   const [btnSave, setBtnSave] = useState(true);
   const [isOverflowed, setIsOverflowed] = useState<boolean>(false);
-  
-  const [morebutton, setmorebutton] = useState('see more');
-  const descriptionRef  = useRef<any>([null])
 
-  const [overflowStatus, setOverflowStatus] = useState<{ [key: number]: boolean }>({});
+  const [morebutton, setmorebutton] = useState("see more");
+  const descriptionRef = useRef<any>([null]);
 
+  const [overflowStatus, setOverflowStatus] = useState<{
+    [key: number]: boolean;
+  }>({});
+
+  const [moreButtons, setMoreButtons] = useState<{ [key: number]: boolean }>(
+    {}
+  );
 
   const fetchData = async () => {
-    const api_url = "http://localhost:3001/todos" 
+    const api_url = "http://localhost:3001/todos";
     try {
       const response = await fetch(api_url); // Fetch data from the API route
       data = await response.json();
-      // console.log(data);
       setTodos(data);
-      // setranderAfter(true)
     } catch (error) {
-      console.error('Error fetching todos:', error);
+      console.error("Error fetching todos:", error);
     }
   };
-  
-    useEffect(() => {
-      if (btnSave){
-        fetchData();
-        console.log("Date is fetched");
-        setBtnSave(false)
-      }        
+
+  useEffect(() => {
+    if (btnSave) {
+      fetchData();
+      console.log("Date is fetched");
+      setBtnSave(false);
+    }
   }, [btnSave]);
 
-  
-  
-  const  todostatus = async (todoId: number, markAsComplete: boolean) => {
-
-
+  const todostatus = async (todoId: number, markAsComplete: boolean) => {
     const updatedTodos = todos.map((todo) =>
-    // todo.id === todoId ? { ...todo, completed: markAsComplete } : todo
+      // todo.id === todoId ? { ...todo, completed: markAsComplete } : todo
       todo.id === todoId ? { ...todo, status: markAsComplete } : todo
     );
 
     setTodos(updatedTodos);
-    // setranderAfter(true)
     try {
       const api_url = `http://localhost:3001/todos/${todoId}`;
       await fetch(api_url, {
-        method: 'PATCH', // or 'PUT'
+        method: "PATCH", // or 'PUT'
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ status: markAsComplete }),
       });
     } catch (error) {
-      console.error('Error updating todo status:', error);
+      console.error("Error updating todo status:", error);
     }
   };
-//delete task
+  //delete task
   const deleteTask = async (todoId: number) => {
-
     const updatedTodos = todos.filter((todo) => todo.id !== todoId);
     setTodos(updatedTodos);
-    // setranderAfter(true)
     try {
       const api_url = `http://localhost:3001/todos/${todoId}`;
       await fetch(api_url, {
-        method: 'DELETE', // or 'PUT'
+        method: "DELETE", // or 'PUT'
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       });
     } catch (error) {
-      console.error('Error updating todo status:', error);
+      console.error("Error updating todo status:", error);
     }
-
-  }
+  };
   const editTask = async (todoId: number) => {
-    // setShouldRerender(true);
-    
-    // setranderAfter((prev) => !prev);
-    let forEditing: any ;
+    let forEditing: any;
 
     try {
       const api_url = `http://localhost:3001/todos/${todoId}`;
       const response = await fetch(api_url);
-      // console.log(response.description);
       forEditing = await response.json();
-      // console.log("err:" , forEditing);
     } catch (error) {
-      console.error('Error updating todo status:', error);
+      console.error("Error updating todo status:", error);
     }
     // return forEditing
     console.log("forEditing:", forEditing);
-    setbtnState(true)
-    seteditData(forEditing)
-    setbtnId(todoId)
-    setFormVisible(true)
-    // setranderAfter(true)   
+    setbtnState(true);
+    seteditData(forEditing);
+    setbtnId(todoId);
+    setFormVisible(true);
+  };
+
+  function activeSeemore(todoId: number) {
+    setMoreButtons((prevButtons) => ({
+      ...prevButtons,
+      [todoId]: !prevButtons[todoId],
+    }));
   }
-  
- 
-function activeSeemore(morebutton:string){
-  if (morebutton == "see more"){
-    // setIsOverflowed(!isOverflowed)
-    setmorebutton("see less")
-  }else{
-    setmorebutton("see more")
-  }
-}
 
-useEffect(() => {
-  todos.map((todo) => {
-    const descriptionElement = descriptionRef.current[todo.id];
-    console.log("🚀 ~ file: status.tsx:146 ~ todos.map ~ descriptionElement:", descriptionElement)
-  if (descriptionElement) {
-    const overflowed = descriptionElement.scrollHeight > descriptionElement.clientHeight;
-    setIsOverflowed(overflowed);
-    setOverflowStatus((prevStatus) => ({ ...prevStatus, [todo.id]: overflowed }));
-    console.log("🚀 ~ file: status.tsx:156 ~ todos.map ~ overflowed:", overflowed)
-    console.log('scroll',descriptionElement.scrollHeight)
-    console.log('client', descriptionElement.clientHeight)
-  }else{
-    console.log("Element not found");
-  }
-})
-}, [todos]);
-
-
-
+  useEffect(() => {
+    todos.map((todo) => {
+      const descriptionElement = descriptionRef.current[todo.id];
+      console.log(
+        "🚀 ~ file: status.tsx:146 ~ todos.map ~ descriptionElement:",
+        descriptionElement
+      );
+      if (descriptionElement) {
+        const overflowed =
+          descriptionElement.scrollHeight > descriptionElement.clientHeight;
+        setIsOverflowed(overflowed);
+        setOverflowStatus((prevStatus) => ({
+          ...prevStatus,
+          [todo.id]: overflowed,
+        }));
+        console.log(
+          "🚀 ~ file: status.tsx:156 ~ todos.map ~ overflowed:",
+          overflowed
+        );
+        console.log("scroll", descriptionElement.scrollHeight);
+        console.log("client", descriptionElement.clientHeight);
+      } else {
+        console.log("Element not found");
+      }
+    });
+  }, [todos]);
 
   return (
     <div>
-      
       <div>
-        {Array.isArray(todos) && todos.length > 0 && props.todoList === 'pending' ? (
-          todos.map((todo) => (
-            !todo.status && (
-              <div key={todo.id} className='py-2'>
-                {/* ======================================show of edit inline */}
-              {(btnState && btnId === todo.id && formVisible) ? (
-                  <Newtask btnS = {()=>setBtnSave(true)} btnState={btnState} editData={editData} onCancel={() => {setFormVisible(false);}} />
-                ):          
-              //use effect for showing see more button
-              <div className="bg-slate-800 "> 
-              <div className='px-2'>
-              <h3 className='break-words'>{todo.title}</h3>
+        {Array.isArray(todos) &&
+        todos.length > 0 &&
+        props.todoList === "pending" ? (
+          todos.map(
+            (todo) =>
+              !todo.status && (
+                <div key={todo.id} className="py-2">
+                  {/* ======================================show of edit inline */}
+                  {btnState && btnId === todo.id && formVisible ? (
+                    <Newtask
+                      btnS={() => setBtnSave(true)}
+                      btnState={btnState}
+                      editData={editData}
+                      onCancel={() => {
+                        setFormVisible(false);
+                      }}
+                    />
+                  ) : (
+                    //use effect for showing see more button
+                    <div className="bg-slate-800 ">
+                      <div className="px-2">
+                        <h3 className="break-words">{todo.title}</h3>
 
-              {/* <div > */}
-              {/* <p  ref={descriptionRef} className='c  line-clamp-3  overflow-hidden whitespace-normal overflow-ellipsis'>{todo.description}</p> */}
-              {/* <p  ref={descriptionRef} className={`overflow-hidden isOverflowed whitespace-normal ${isOverflowed ? 'overflow-ellipsis line-clamp-2' : 'line-clamp-none'}`}>{todo.description}</p> */}
-              {/* <p  ref={descriptionRef} className='break-words line-clamp-2'>{todo.description}</p> */}
-              {/* </div> */}
+                        <p
+                          ref={(el) => (descriptionRef.current[todo.id] = el!)}
+                          className={`break-words ${
+                            moreButtons[todo.id] ? "" : "line-clamp-2"
+                          }`}
+                        >
+                          {todo.description}
+                        </p>
 
-              <p ref={el => (descriptionRef.current[todo.id] = el!)} className={`break-words ${(morebutton == 'see more') ? ' line-clamp-2' : 'line-clamp-none'}`}>{todo.description}</p>
-                        
-              
-              {(overflowStatus[todo.id]) && (<div className='my-2 text-xs  text-blue-800 hover:text-blue-500  flex justify-end'>
-              <button className=' ml-4 ' onClick={()=>activeSeemore(morebutton)}>{morebutton}</button>
-              </div>)}
+                        {overflowStatus[todo.id] && (
+                          <div className="my-2 text-xs  text-blue-800 hover:text-blue-500  flex justify-end">
+                            <button
+                              className="ml-4"
+                              onClick={() => activeSeemore(todo.id)}
+                            >
+                              {moreButtons[todo.id] ? "see less" : "see more"}
+                            </button>
+                          </div>
+                        )}
+                      </div>
 
-              </div>
-              
-              <div className='flex justify-between items-end'>
-                <div className='flex items-center ml-2  py-1' >
-              <button onClick={() => deleteTask(todo.id)}>
-              <RiDeleteBinLine  className='h-4 w-4  text-red-500 hover:text-red-600'  />
-              </button>
-              <button onClick={() =>editTask(todo.id)}>
-              <FiEdit className='h-4 w-4 mx-2  text-red-500 hover:text-red-600' />
-              </button  >
+                      <div className="flex justify-between items-end">
+                        <div className="flex items-center ml-2  py-1">
+                          <button onClick={() => deleteTask(todo.id)}>
+                            <RiDeleteBinLine className="h-4 w-4  text-red-500 hover:text-red-600" />
+                          </button>
+                          <button onClick={() => editTask(todo.id)}>
+                            <FiEdit className="h-4 w-4 mx-2  text-red-500 hover:text-red-600" />
+                          </button>
+                        </div>
+                        <Button
+                          variant="default"
+                          onClick={() => todostatus(todo.id, true)}
+                          className="bg-green-700 hover:bg-green-700  text-black hover:text-white rounded-none "
+                        >
+                          Completed
+                        </Button>
+                      </div>
+                    </div>
+                  )}
                 </div>
-              <Button variant='default' onClick={() => todostatus(todo.id,true)} className='bg-green-700 hover:bg-green-700  text-black hover:text-white rounded-none ' >Completed</Button>
-              </div>
-              </div>
-        }
-            </div>
-            )
-            ))
-        ) :((Array.isArray(todos) && todos.length > 0 && props.todoList === 'completed')?
-          (
-            todos.map((todo) => (
-              todo.status && (
-                <div key={todo.id} className='py-2'>
-              {/* ======================================show of edit inline */} 
-
-
-              {(btnState && btnId === todo.id && formVisible) ? (
-                  <Newtask btnS = {()=>setBtnSave(true)} btnState={btnState} editData={editData} onCancel={() => {setFormVisible(false);}} />
-                ):          
-              
-              <div className="bg-slate-800 "> 
-              <div className='px-2'>
-              <h3 >{todo.title}</h3>
-              <p>{todo.description}</p>
-              
-              </div>
-              
-              <div className='flex justify-between items-end'>
-                <div className='flex items-center ml-2  py-1' >
-              <button onClick={() => deleteTask(todo.id)}>
-              <RiDeleteBinLine  className='h-4 w-4  text-red-500 hover:text-red-600'  />
-              </button>
-              <button onClick={() =>editTask(todo.id)}>
-              <FiEdit className='h-4 w-4 mx-2  text-red-500 hover:text-red-600' />
-              </button  >
-                </div>
-              <Button variant='default' onClick={() => todostatus(todo.id,false)} className='bg-red-700 hover:bg-red-700  text-black hover:text-white rounded-none ' >To Do</Button>
-              </div>
-              </div>
-        }
-
-                
-              </div>
               )
-            ))
-          ):(
-            <p>No Task Exist</p>
           )
-        ) 
-        }
+        ) : Array.isArray(todos) &&
+          todos.length > 0 &&
+          props.todoList === "completed" ? (
+          todos.map(
+            (todo) =>
+              todo.status && (
+                <div key={todo.id} className="py-2">
+                  {/* ======================================show of edit inline */}
+
+                  {btnState && btnId === todo.id && formVisible ? (
+                    <Newtask
+                      btnS={() => setBtnSave(true)}
+                      btnState={btnState}
+                      editData={editData}
+                      onCancel={() => {
+                        setFormVisible(false);
+                      }}
+                    />
+                  ) : (
+                    <div className="bg-slate-800 ">
+                      <div className="px-2">
+                        <h3>{todo.title}</h3>
+                        <p>{todo.description}</p>
+                      </div>
+
+                      <div className="flex justify-between items-end">
+                        <div className="flex items-center ml-2  py-1">
+                          <button onClick={() => deleteTask(todo.id)}>
+                            <RiDeleteBinLine className="h-4 w-4  text-red-500 hover:text-red-600" />
+                          </button>
+                          <button onClick={() => editTask(todo.id)}>
+                            <FiEdit className="h-4 w-4 mx-2  text-red-500 hover:text-red-600" />
+                          </button>
+                        </div>
+                        <Button
+                          variant="default"
+                          onClick={() => todostatus(todo.id, false)}
+                          className="bg-red-700 hover:bg-red-700  text-black hover:text-white rounded-none "
+                        >
+                          To Do
+                        </Button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              )
+          )
+        ) : (
+          <p>No Task Exist</p>
+        )}
       </div>
-  
     </div>
   );
 };
 
 export default TodoList;
-
--- Quarter 02/project_nextjs/src/app/todo/page.tsx
+++ Quarter 02/project_nextjs/src/app/todo/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import TodoList from "./components/status";
@@ -6,21 +6,13 @@ import { useState } from "react";
 import Newtask from "./components/newtask";
 // import editTask from "./components/editTask";
 
-
-
-
 export default function Todo() {
-
-
   const [todoList, setTodoList] = useState("pending");
   const [divVisible, setDivVisible] = useState(false);
   // const [newTask, setNewTask] = useState(false);
   const [rerender, setRerender] = useState(true);
   const [mainSave, setmainSave] = useState(false);
 
-
-  
-  
   return (
     <div className="flex flex-col items-center justify-center py-16">
       <div className="flex flex-col bg-white py-0.5 px-0.5 rounded-md w-2/5">
@@ -31,37 +23,63 @@ export default function Todo() {
           {/*====================== Main Div for working ====================*/}
           {/*============================= Start ============================*/}
           <div className=" w-full ">
-          <Tabs defaultValue={todoList} onValueChange={(value:string) => {setTodoList(value); setDivVisible(false)}} className="w-full text-white py-1 px-1 ">
-          <div className="flex justify-between flex-wrap">
-              <div>
-                <TabsList className="bg-slate-900 text-white flex flex-wrap" >
-                  <TabsTrigger value="pending" className="bg-slate-900 text-slate-400">Pending</TabsTrigger>
-                  <TabsTrigger value="completed" className="bg-slate-900 text-slate-400">Completed</TabsTrigger>
-                </TabsList>
+            <Tabs
+              defaultValue={todoList}
+              onValueChange={(value: string) => {
+                setTodoList(value);
+                setDivVisible(false);
+              }}
+              className="w-full text-white py-1 px-1 "
+            >
+              <div className="flex justify-between flex-wrap">
+                <div>
+                  <TabsList className="bg-slate-900 text-white flex flex-wrap">
+                    <TabsTrigger
+                      value="pending"
+                      className="bg-slate-900 text-slate-400"
+                    >
+                      Pending
+                    </TabsTrigger>
+                    <TabsTrigger
+                      value="completed"
+                      className="bg-slate-900 text-slate-400"
+                    >
+                      Completed
+                    </TabsTrigger>
+                  </TabsList>
+                </div>
+                <div className="">
+                  <Button
+                    variant={"secondary"}
+                    onClick={() => setmainSave(true)}
+                    className=" my-1 bg-green-700 hover:bg-green-700 text-black hover:text-white"
+                  >
+                    {" "}
+                    Add Task
+                  </Button>
+                </div>
               </div>
-              <div className="">
-                <Button variant={"secondary"} onClick={() => setmainSave(true)} className=" my-1 bg-green-700 hover:bg-green-700 text-black hover:text-white"> Add Task</Button>
-              </div>
-          </div>
-          {(!mainSave) &&  <div>
-            <TabsContent value="pending">
-                {/*==========================================================Pending==*/}
-              
-              {rerender && <TodoList todoList={todoList} />}
-            </TabsContent>
-            <TabsContent value="completed">
-                {/*==========================================================Pending==*/}
-                {rerender && <TodoList todoList={todoList} />}
-            </TabsContent>
-          </div>
-          }
-          {( mainSave ) && 
-            // <Newtask newTaskRender = {()=>setNewTask(true)}/>
-            <Newtask main={()=>setmainSave(false)}/>
-            // setmainSave(true)
-          }
-          
-          </Tabs>
+              {!mainSave && (
+                <div>
+                  <TabsContent value="pending">
+                    {/*==========================================================Pending==*/}
+
+                    {rerender && <TodoList todoList={todoList} />}
+                  </TabsContent>
+                  <TabsContent value="completed">
+                    {/*==========================================================Pending==*/}
+                    {rerender && <TodoList todoList={todoList} />}
+                  </TabsContent>
+                </div>
+              )}
+              {
+                mainSave && (
+                  // <Newtask newTaskRender = {()=>setNewTask(true)}/>
+                  <Newtask main={() => setmainSave(false)} />
+                )
+                // setmainSave(true)
+              }
+            </Tabs>
           </div>
           {/*============================== End =============================*/}
           {/*====================== Main Div for working ====================*/}
